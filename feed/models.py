1f421e2d1c3cb15666742c7ad11469fbbf00f299
Bill Anton <94488468+DogDad65@users.noreply.github.com>
2022-09-01 11:39:42 -0400

    Delete feed directory

--- feed/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from sorl.thumbnail import ImageField
-
-class Post(models.Model):
-   text = models.CharField(max_length=140, blank=False, null=False)
-   image = ImageField(upload_to='')
-   def __str__(self):
-      return self.text
